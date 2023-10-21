352f2e8181d65ef036a8729c2035dc3659dbcb29
daryllzj <daryllzj@gmail.com>
2023-10-21 23:17:54 +0800

    created MusicPlayerCLI.java

--- src/MusicPlayer/MusicPlayer.java
+++ src/MusicPlayer/MusicPlayer.java
@@ -1,14 +1,33 @@
 package MusicPlayer;
 
-import javax.sound.midi.MidiSystem;
-import javax.sound.midi.Sequencer;
+import javax.sound.midi.*;
 
 public class MusicPlayer {
 
     public  void play(){
         try {
             Sequencer sequencer = MidiSystem.getSequencer();
-            System.out.println("got sequencer");
+            sequencer.open();
+
+            Sequence sequence = new Sequence(Sequence.PPQ,4);
+
+            Track track = sequence.createTrack();
+
+            ShortMessage msg1 = new ShortMessage();
+            msg1.setMessage(ShortMessage.NOTE_ON, 1, 102, 100);
+            MidiEvent noteOn = new MidiEvent(msg1,1);
+            track.add(noteOn);
+
+            ShortMessage msg2 = new ShortMessage();
+            msg1.setMessage(ShortMessage.NOTE_OFF, 1, 30, 100);
+            MidiEvent noteOff = new MidiEvent(msg2,16);
+            track.add(noteOff);
+
+            sequencer.setSequence(sequence);
+
+            sequencer.start();
+
+
         } catch (Exception e) {
             System.out.println(e.getLocalizedMessage());
         }
@@ -16,6 +35,7 @@ public class MusicPlayer {
 
     public static void main(String[] args) {
         MusicPlayer musicPlayer = new MusicPlayer();
+        System.out.println("starting music");
         musicPlayer.play();
     }
 }
---
+++ src/MusicPlayer/MusicPlayerCLI.java
@@ -0,0 +1,55 @@
+package MusicPlayer;
+
+import javax.sound.midi.*;
+
+public class MusicPlayerCLI {
+
+    public static void main(String[] args) {
+        MusicPlayerCLI musicPlayer = new MusicPlayerCLI();
+
+        System.out.println("starting music");
+
+        if (args.length < 2){
+            System.out.println("add args");
+        } else {
+            int instrument = Integer.parseInt(args[0]);
+            int note = Integer.parseInt(args[1]);
+            musicPlayer.play(instrument, note);
+        }
+
+        System.out.println("ending music");
+    }
+
+    public  void play(int instrument, int note){
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+
+            Sequence sequence = new Sequence(Sequence.PPQ,4);
+            Track track = sequence.createTrack();
+
+            ShortMessage msg1 = new ShortMessage();
+            msg1.setMessage(ShortMessage.PROGRAM_CHANGE, 1, instrument, 0);
+            MidiEvent changeInstrument = new MidiEvent(msg1,1);
+            track.add(changeInstrument);
+
+            ShortMessage msg2 = new ShortMessage();
+            msg1.setMessage(ShortMessage.NOTE_OFF, 1, note, 100);
+            MidiEvent noteOn = new MidiEvent(msg2,1);
+            track.add(noteOn);
+
+            ShortMessage msg3 = new ShortMessage();
+            msg1.setMessage(ShortMessage.NOTE_OFF, 1, note, 100);
+            MidiEvent noteOff = new MidiEvent(msg3,16);
+            track.add(noteOff);
+
+            sequencer.setSequence(sequence);
+            sequencer.start();
+
+
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+
+}
