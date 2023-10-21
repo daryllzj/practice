386f21679c5cbb8fdf4ddd26e1464ee7d81f62a7
daryllzj <daryllzj@gmail.com>
2023-10-21 22:28:39 +0800

    setting up MusicPlayer.java

--- src/MusicPlayer/MusicPlayer.java
+++ src/MusicPlayer/MusicPlayer.java
@@ -1,4 +1,21 @@
 package MusicPlayer;
 
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequencer;
+
 public class MusicPlayer {
+
+    public  void play(){
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+            System.out.println("got sequencer");
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        MusicPlayer musicPlayer = new MusicPlayer();
+        musicPlayer.play();
+    }
 }
